07ddd0c07b13e8948f53b4d317a9d5c827ff976d
mahmad-alaadwy <104855133+mahmad-alaadwy@users.noreply.github.com>
2023-03-04 00:32:26 +0200

    initial commit

---
+++ OddEvenNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbs = { 1, 2, 3, 4 };
+
+            Console.WriteLine("Hello World!");
+            printNumbers("the number in the array are  ", numbs);
+        }
+
+        static void printNumbers(string title,IEnumerable<int> numbs)
+        {
+            Console.WriteLine();
+            Console.Write($"{title}: [");
+            foreach(var i in numbs)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.Write($" ]");
+            Console.WriteLine();
+        }
+    }
+}
